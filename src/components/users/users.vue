a0a71e5562a13a292cdceabf72f7a6c6d4a75fe0
flora <736536764@qq.com>
2020-09-10 16:10:55 +0800

    分配角色

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -9,10 +9,10 @@
   <!--搜索-->
   <el-row class="searchRow">
     <el-col>
-      <el-input placeholder="请输入内容" v-model="query" class="inputSelect">
-        <el-button slot="append" icon="el-icon-search"></el-button>
+      <el-input @clear="loadUserList()" clearable placeholder="请输入内容" v-model="query" class="inputSelect">
+        <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
       </el-input>
-      <el-button type="success">添加用户</el-button>
+      <el-button type="success" @click="showAddUserDia()">添加用户</el-button>
     </el-col>
   </el-row>
 
@@ -40,19 +40,96 @@
     </el-table-column>
     <el-table-column label="用户状态">
       <template slot-scope="scope">
-        <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949">
+        <el-switch 
+        @change="changeMgState(scope.row)"
+        v-model="scope.row.mg_state" 
+        active-color="#13ce66" 
+        inactive-color="#ff4949">
         </el-switch>
       </template>
     </el-table-column>
     <el-table-column label="操作">
       <template slot-scope="scope">
-        <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
-        <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
-        <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
-      </template> 
-    </el-table-column> 
-  </el-table> 
+        <el-button @click="showEditMsgBox(scope.row)" size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
+        <el-button @click="showSetUserBox(scope.row)" size="mini" plain type="success" icon="el-icon-check" circle></el-button>
+        <el-button @click="showDeleteMsgBox(scope.row.id)" size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+      </template>
+    </el-table-column>
+  </el-table>
   <!--分页-->
+  <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum" :page-sizes="[2, 4, 6, 8]" :page-size="pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total">
+  </el-pagination>
+
+  <!--对话框-->
+  <!--1.添加用户的对话框-->
+  <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+    <el-form :model="form">
+      <el-form-item label="用户名" label-width="100px">
+        <el-input v-model="form.username" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="密码" label-width="100px">
+        <el-input v-model="form.password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="邮箱" label-width="100px">
+        <el-input v-model="form.email" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="电话" label-width="100px">
+        <el-input v-model="form.mobile" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+      <el-button type="primary" @click="addUser()">确 定</el-button>
+    </div>
+  </el-dialog>
+
+  <!--2.编辑用户的对话框-->
+  <el-dialog title="编辑用户" :visible.sync="dialogFormVisibleEdit">
+    <el-form :model="form">
+      <el-form-item label="用户名" label-width="100px">
+        <el-input disabled v-model="form.username" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="邮箱" label-width="100px">
+        <el-input v-model="form.email" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item label="电话" label-width="100px">
+        <el-input v-model="form.mobile" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+      <el-button type="primary" @click="editUser()">确 定</el-button>
+    </div>
+  </el-dialog>
+
+  <!--3.分配角色的对话框-->
+  <el-dialog title="分配角色" :visible.sync="dialogFormVisibleRol">
+  <el-form :model="form">
+    <el-form-item label="用户名" label-width="100px">
+      {{curUserName}}
+    </el-form-item>
+    <el-form-item label="角色" label-width="100px">
+      <el-select v-model="curRoleId">
+        <el-option label="请选择" :value="-1"></el-option>
+        <el-option 
+        :label="item.roleName" 
+        :value="item.id"
+        v-for="(item, i) in roles"
+        :key="i"
+        ></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisibleRol = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
 </el-card>
 </template>
 
@@ -66,13 +143,151 @@ export default {
       // 分页相关的数据
       total: -1,
       pagenum: 1,
-      pagesize: 2
+      pagesize: 2,
+      // 添加对话框属性
+      dialogFormVisibleAdd: false,
+      // 编辑对话框属性
+      dialogFormVisibleEdit: false,
+      // 分配角色对话框属性
+      dialogFormVisibleRol: false,
+      // 添加用户的表单数据
+      form: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      curRoleId: -1,
+      curUserName: '',
+      //保存所有角色数据
+      roles: []
     }
   },
   created() {
     this.getUserList()
   },
   methods: {
+    // 分配角色
+    async showSetUserBox(user) {
+      this.curUserName = user.username
+
+      // 获取所有角色
+      const res1 = await this.$http.get(`roles`)
+      console.log(res1)
+      this.roles = res1.data.data
+
+      // 获取当前用户的角色id ->rid
+      const res = await this.$http.get(`users/${user.id}`)
+      this.curRoleId = res.data.data.rid
+
+      this.dialogFormVisibleRol = true
+    },
+    // 修改状态
+    async changeMgState(user) {
+      const res = await this.$http.put(`users/${user.id}/state/${user.mg_state}`)
+      console.log(res)
+    },
+    // 编辑用户 - 发送请求
+    async editUser() {
+      const res = await this.$http.put(`users/${this.form.id}`, this.form)
+      if (res.data.meta.status === 200) {
+        // 关闭对话框
+        this.dialogFormVisibleEdit = false
+        // 更新视图
+        this.getUserList()
+        this.$message.success("编辑成功")
+      } else {
+        this.$message.warning("编辑失败")
+      }
+
+    },
+    // 编辑用户 - 显示对话框
+    showEditMsgBox(user) {
+      // 获取用户数据
+      this.form = user
+      this.dialogFormVisibleEdit = true
+    },
+    // 删除用户 - 打开消息盒子
+    showDeleteMsgBox(userID) {
+      this.$confirm('删除用户?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 发送删除请求
+        // data中找userid
+        // 把userid以参数形式传进来
+        const res = await this.$http.delete(`users/${userID}`)
+        if (res.data.meta.status === 200) {
+          // 更新视图
+          this.pagenum = 1
+          this.getUserList()
+          // 提示
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        } else {
+          this.$message.warning("删除失败")
+        }
+
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    // 添加用户 发送请求
+    async addUser() {
+      // 2.关闭对话框
+      this.dialogFormVisibleAdd = false
+
+      const res = await this.$http.post(`users`, this.form)
+      console.log(res)
+      const {
+        meta: {
+          status,
+          msg
+        },
+        data
+      } = res.data
+      if (status === 201) {
+        // 1.提示
+        this.$message.success(msg)
+        // 3.刷新视图
+        this.getUserList()
+        // 4.清空文本框
+        this.form = {}
+      } else {
+        this.$message.warning(msg)
+      }
+    },
+    // 显示添加用户对话框
+    showAddUserDia() {
+      this.form = {}
+      this.dialogFormVisibleAdd = true
+    },
+    // 清除搜索框 重新获取数据
+    loadUserList() {
+      this.getUserList()
+    },
+    // 搜索用户
+    searchUser() {
+      this.getUserList()
+    },
+    // 分页相关方法
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pagesize = val
+      this.pagenum = 1
+      this.getUserList()
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pagenum = val
+      this.getUserList()
+    },
     // 获取用户列表请求
     async getUserList() {
       // 需要授权的API,必须在请求头中使用Authorization字段提供token令牌
